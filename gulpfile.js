96b7c7c19a137b0c50f86558457a5d488b59c10e
gauravshingala <124760696+gauravshingala@users.noreply.github.com>
2023-03-25 17:49:35 +0530

    changes alert & avtars page

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ gulp.task('browser-sync', async function (done) {
   browsersync.init({
     base: './',
     server: './',
-    startPath: 'html/unique/index.html',
+    startPath: 'html/friendly-layout/index.html',
     host: 'localhost',
     open: true,
     tunnel: false,
