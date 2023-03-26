f1e43d0b408ca9b6c4841bc6d695c077796e57af
Nidhi-kanthariya <nidhikanthariya99@gmail.com>
2023-03-26 22:07:05 +0530

    add icon sidebar css
    -- made some changes in login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -305,6 +305,26 @@ for (let i = 0; i < showHideBtn.length; ++i) {
   });
 }
 
+/*=====================
+Password hide/show js
+==========================*/
+
+const eye = document.querySelector(".feather-eye");
+const eyeoff = document.querySelector(".feather-eye-off");
+const passwordField = document.querySelector("input[type=password]");
+
+eye.addEventListener("click", () => {
+  eye.style.display = "none";
+  eyeoff.style.display = "block";
+  passwordField.type = "text";
+});
+
+eyeoff.addEventListener("click", () => {
+  eyeoff.style.display = "none";
+  eye.style.display = "block";
+  passwordField.type = "password";
+});
+
 
 /*=====================
  Close alert js
@@ -381,3 +401,8 @@ suggest?.forEach((el) => {
     suggestButton.style.padding = "0px";
   });
 });
+
+
+
+
+
